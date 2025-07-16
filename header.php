6b5833015511ae652fd67be0c5f999187dbd0bf0
Euler <euler.codinpro@gmail.com>
2025-07-16 14:06:38 +0100

    premier commit du projet

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Realtime Chat App | CodingNepal</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+</head>
+<body>
+ <nav class="navbar">
+  <div class="navbar-logo">
+    <a href="index.php"><i class="fas fa-comments"></i> ChatApp</a>
+  </div>
+  <div class="navbar-toggle" id="navbar-toggle">
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+  <ul class="navbar-links" id="navbar-links">
+    <li><a href="index.php"><i class="fas fa-home"></i> Accueil</a></li>
+    <li><a href="users.php"><i class="fas fa-users"></i> Utilisateurs</a></li>
+    <li><a href="login.php"><i class="fas fa-sign-in-alt"></i> Connexion</a></li>
+    <li><a href="logout.php"><i class="fas fa-sign-out-alt"></i> Déconnexion</a></li>
+  </ul>
+</nav>
+
---
+++ topbar.php
@@ -0,0 +1,12 @@
+<div class="topbar">
+	<a href="index.php" class="app-name">Tchat</a>
+	<span class="menu">
+		
+		<a href="index.php?page=membres" class="active">Membres</a>
+		<a href="index.php?page=logout">Déconnexion</a>
+		
+		<a href="index.php?page=register">S'inscrire</a>
+		<a href="index.php?page=signin" >Se connecter</a>
+		
+	</span>
+</div>
